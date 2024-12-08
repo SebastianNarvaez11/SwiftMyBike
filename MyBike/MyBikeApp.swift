3d6b1fb7ff8224fe1f8f57fe974f28279ff23fd3
Sebastian Narvaez <narvaez.jhoan@correounivalle.edu.co>
2024-12-08 14:03:39 -0500

    Initial Commit

---
+++ MyBike/MyBikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBikeApp.swift
+//  MyBike
+//
+//  Created by Sebastian Narvaez on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MyBikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
